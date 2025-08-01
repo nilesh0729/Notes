807a1275680373a356c042f00ad2ba8112a3d243
Nilesh Khodiyar <nileshkhodiyar2000@gmail.com>
2025-08-01 11:06:21 +0000

    added user Query file (user.sql)

---
+++ db/Query/user.sql
@@ -0,0 +1,34 @@
+-- name: CreateUser :one
+INSERT INTO users (
+  username,
+  email,
+  password_hash
+) VALUES (
+  $1, $2, $3
+)
+RETURNING *;
+
+
+-- name: GetUsers :one
+SELECT * FROM users
+WHERE id = $1 
+LIMIT 1;
+
+
+-- name: ListUsers :many
+SELECT * FROM users
+ORDER BY id
+LIMIT $ 1
+OFFSET $2;
+
+
+-- name: UpdateUsers :exec
+UPDATE users
+  set password_hash = $2,
+  email = $3
+WHERE id = $1;
+
+
+-- name: DeleteUsers :exec
+DELETE FROM users
+WHERE id = $1;
