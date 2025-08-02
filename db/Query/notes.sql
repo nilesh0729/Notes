f69adeecdd645dd0935bc8ecb2c5034be3f8155d
Nilesh Khodiyar <nileshkhodiyar2000@gmail.com>
2025-08-02 12:59:52 +0000

    initiated notes query file with basic queries

---
+++ db/Query/notes.sql
@@ -0,0 +1,36 @@
+-- name: CreateNotes :one
+INSERT INTO notes (
+  user_id,
+  title,
+  content, 
+  pinned,
+  archived
+) VALUES (
+  $1, $2, $3, $4, $5
+)
+RETURNING *;
+
+-- name: GetNotes :one
+SELECT * FROM notes
+WHERE id = $1 
+LIMIT 1;
+
+-- name: ListNotes :many
+SELECT * FROM notes
+WHERE user_id = $1 AND archived = FALSE 
+ORDER BY created_at DESC;
+
+
+-- name: UpdateNotes :one
+UPDATE notes
+  set title = $2,
+      content = $3,
+      pinned = $4,
+      archived = $5,
+      updated_at = NOW()
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteNotes :exec
+DELETE FROM notes
+WHERE id = $1;
