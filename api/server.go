bbdd698c602443075e2395fab6b5adcd879c4466
Nilesh Khodiyar <nileshkhodiyar2000@gmail.com>
2025-10-04 07:15:35 +0000

    initilized server.go for api

---
+++ api/server.go
@@ -0,0 +1,46 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	Database "github.com/nilesh0729/Notes/db/Result"
+)
+
+type Server struct {
+	store  Database.Store
+	router *gin.Engine
+}
+
+func NewServer(store Database.Store) *Server{
+	server := &Server{
+		store : store,
+	}
+	router := gin.Default()
+
+	router.POST("/notes", server.CreateNote)
+	
+	router.GET("/notes/:id", server.GetNoteById)
+
+	router.GET("/notes", server.ListNotes)
+
+	router.POST("/tags", server.CreateTags)
+
+	router.GET("/tags/:id", server.GetTag)
+
+	router.GET("/tags", server.ListTags)
+
+	router.POST("/note_tags", server.AddTagToNote)
+
+	router.GET("/note")
+
+	server.router = router
+
+	return server
+}
+
+func (server *Server) Start(address string) error{
+	return server.router.Run(address)
+}
+
+func errResponse (err error)gin.H{
+	return gin.H{"error" : err.Error() }
+}
