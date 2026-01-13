a737936cba5f8d6cacdcbdf187b084108a1c98aa
Nilesh Khodiyar <nileshkhodiyar2000@gmail.com>
2026-01-13 22:53:46 +0530

    Updated config

--- internal/util/config.go
+++ internal/util/config.go
@@ -22,6 +22,14 @@ func LoadConfig(path string) (config Config, err error) {
 	viper.SetConfigName(".env")
 
 	viper.AutomaticEnv()
+	
+	// Explicitly bind environment variables for cloud deployments
+	// This ensures Viper reads env vars even when .env file doesn't exist
+	viper.BindEnv("DB_DRIVER")
+	viper.BindEnv("DB_SOURCE")
+	viper.BindEnv("SERVER_ADDRESS")
+	viper.BindEnv("PASSWORD")
+	viper.BindEnv("ACCESS_TOKEN_DURATION")
 
 	// Try to read config file, but ignore if it doesn't exist
 	// This allows the app to work with environment variables only (e.g., in cloud deployments)
